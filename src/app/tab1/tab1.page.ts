22aa6d44d9d674093968b57732cae2c35328820d
egonbognar <egonbognar@gmail.com>
2022-05-08 12:41:51 +0200

    update

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, Injectable} from '@angular/core';
+import 'heartbeat-plugin';
+import {HeartbeatPluginWeb} from "heartbeat-plugin/src/web";
+import {registerPlugin} from "@capacitor/core";
+import {Plugins} from "@capacitor/core";
+const {HeartbeatPluginPlugin} = Plugins;
+//import {HeartbeatPlugin} from "heartbeat-plugin";
+
+//erre dob errort
+//import {HeartbeatPlugin} from "../../../android/app/src/main/java/io/ionic/starter"
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
 
-  constructor() {}
+export class Tab1Page{
+asd: any;
+  constructor() {
+  }
+
+  kiir() {
+   // HeartbeatPlugin.proba('asd')
+    //this.asd = HeartbeatPlugin.proba('asd').value;
+    HeartbeatPluginPlugin.proba({asdd: 'valami'});
+   // HeartbeatPlugin.echo({value: 'asd'})
+  }
+
+
 
 }
